f6f229e2db2f949750ba3037bb24d535a67542e7
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-16 11:21:45 +0100

    réalisation challenge 2

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const username = "John";
 let admin = username;
 console.log(`Résultat du challenge 1:`);
 console.log(`Admin est égale à ${admin}`);
+
+//challenge 2 donkey
+let a = 1,
+	b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
