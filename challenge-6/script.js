4acf09416b85261efaecb9fdc3dc7d042d323b26
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-19 12:29:57 +0100

    début challenge 6

---
+++ challenge-6/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//challenge 69
+
+let scoreDauphin1 = [44, 23, 71];
+let scoreKoalas1 = [65, 54, 49];
+let scoreDauphin2 = [85, 54, 41];
+let scoreKoalas2 = [23, 34, 27];
+
+const calcAverage = (arrayScore) => {
+	let somme = 0;
+	let j = arrayScore.length;
+	for (let i = 0; i < j; i++) {
+		somme += arrayScore[i];
+	}
+
+	return somme / arrayScore.length;
+};
+
+let averageDauphin1 = calcAverage(scoreDauphin1);
+let averageDauphin2 = calcAverage(scoreDauphin2);
+let averageKoalas1 = calcAverage(scoreKoalas1);
+let averageKoalas2 = calcAverage(scoreKoalas2);
+
+console.log(averageDauphin1, averageDauphin2, averageKoalas1, averageKoalas2);
